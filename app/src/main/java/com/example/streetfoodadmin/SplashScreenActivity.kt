691abad6fc721374159068d5f7d677ee4d3f9fe1
aman1 <aman15220@gmail.com>
2024-07-04 01:38:42 +0530

    feat: initialise splash screen for the application

---
+++ app/src/main/java/com/example/streetfoodadmin/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.example.streetfoodadmin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/*
+    This activity implements the splash screen of the application
+ */
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
